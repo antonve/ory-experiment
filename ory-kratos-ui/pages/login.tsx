9c6a7b3095364ba9cc685c9676a5edefe659d981
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-09-15 19:38:24 +0900

    add login form

--- ory-kratos-ui/pages/login.tsx
+++ ory-kratos-ui/pages/login.tsx
@@ -1,7 +1,41 @@
 import type { NextPage } from 'next'
+import { useForm } from 'react-hook-form'
 
 const Login: NextPage = () => {
-  return <div><h1>Log in</h1></div>
+  const { register, handleSubmit, formState } = useForm({
+    defaultValues: {
+      email: 'test@example.com',
+      password: 'foobar',
+    },
+  })
+
+  const onSubmit = (data: any) => console.log(data, formState)
+
+  return (
+    <div>
+      <h1>Log in</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label style={{ display: 'block' }} htmlFor="email">
+          Email
+        </label>
+        <input id="email" {...register('email', { required: true })} />
+
+        <label style={{ display: 'block' }} htmlFor="password">
+          Password
+        </label>
+        <input
+          id="password"
+          type="password"
+          {...register('password', { required: true })}
+        />
+
+        <br />
+
+        <input type="submit" />
+      </form>
+    </div>
+  )
 }
 
 export default Login
