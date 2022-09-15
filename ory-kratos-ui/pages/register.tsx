3afb0e8a13d90902a53409dbac25a2350c3c249d
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-09-15 19:40:18 +0900

    add register form

--- ory-kratos-ui/pages/register.tsx
+++ ory-kratos-ui/pages/register.tsx
@@ -1,9 +1,48 @@
 import type { NextPage } from 'next'
+import { useForm } from 'react-hook-form'
 
 const Register: NextPage = () => {
+  const { register, handleSubmit, formState } = useForm({
+    defaultValues: {
+      email: 'test@example.com',
+      password: 'foobar',
+      displayName: 'John Smith',
+    },
+  })
+
+  const onSubmit = (data: any) => console.log(data, formState)
+
   return (
     <div>
       <h1>Register</h1>
+
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <label style={{ display: 'block' }} htmlFor="email">
+          Email
+        </label>
+        <input id="email" {...register('email', { required: true })} />
+
+        <label style={{ display: 'block' }} htmlFor="password">
+          Password
+        </label>
+        <input
+          id="password"
+          type="password"
+          {...register('password', { required: true })}
+        />
+
+        <label style={{ display: 'block' }} htmlFor="displayName">
+          Display name
+        </label>
+        <input
+          id="displayName"
+          {...register('displayName', { required: true })}
+        />
+
+        <br />
+
+        <input type="submit" />
+      </form>
     </div>
   )
 }
