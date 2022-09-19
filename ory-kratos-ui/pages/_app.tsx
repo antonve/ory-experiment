cf0e86a7a83778316b9f55d5ba4134f36783732d
Anton Van Eechaute <974796+antonve@users.noreply.github.com>
2022-09-19 14:28:15 +0900

    fetch user session on page load

--- ory-kratos-ui/pages/_app.tsx
+++ ory-kratos-ui/pages/_app.tsx
@@ -1,14 +1,34 @@
-import type { AppProps } from 'next/app'
+import App, { AppContext, AppProps } from 'next/app'
 import Link from 'next/link'
+import ory from '../src/ory'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <div>
-      <Link href="/login"><a>Log in</a></Link><br />
-      <Link href="/register"><a>Register</a></Link><hr />
-      <Component {...pageProps} />
-    </div>
-  )
+const MyApp = ({ Component, pageProps }: AppProps) => (
+  <div>
+    <Link href="/login">
+      <a>Log in</a>
+    </Link>
+    <br />
+    <Link href="/register">
+      <a>Register</a>
+    </Link>
+    <hr />
+    <Component {...pageProps} />
+  </div>
+)
+
+MyApp.getInitialProps = async (ctx: AppContext) => {
+  // TODO: Refactor
+  const props = await App.getInitialProps(ctx)
+
+  const cookie = ctx.ctx.req?.headers.cookie
+  if (cookie) {
+    try {
+      const { data: session } = await ory.toSession(undefined, cookie)
+      return { ...props, session }
+    } catch (err) {}
+  }
+
+  return { ...props }
 }
 
 export default MyApp
